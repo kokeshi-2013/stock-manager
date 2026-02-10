31e0fe4c18c8404ef59a11daac99e89f6f527f36
kokeshi-2013 <79235524+kokeshi-2013@users.noreply.github.com>
2026-02-10 22:20:34 +0900

    rakuten-search.tsファイルを作り直し

--- api/rakuten-search.ts
+++ api/rakuten-search.ts
@@ -6,7 +6,6 @@ export default async function handler(
     req: VercelRequest,
     res: VercelResponse
 ) {
-    // CORSヘッダー
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS')
 
